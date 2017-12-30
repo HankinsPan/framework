b1819d1c16ccd9067a155df05b2f36357f3dd49d
Nikolas Burk <nikolas.burk@gmail.com>
2017-12-30 10:35:19 +0100

    rest-wrapper example

--- examples/rest-wrapper/src/index.js
+++ examples/rest-wrapper/src/index.js
@@ -1,45 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { importSchema } = require('graphql-import')
-const { Graphcool } = require('graphcool-binding')
+const { resolvers } = require('./dog-api/resolvers')
 
 const typeDefs = importSchema('./src/schema.graphql')
-const resolvers = {
-  Query: {
-    feed(parent, args, ctx, info) {
-      return ctx.db.query.posts({ where: { isPublished: true } }, info)
-    },
-  },
-  Mutation: {
-    createDraft(parent, { title, text }, ctx, info) {
-      return ctx.db.mutation.createPost(
-        // TODO remove `isPublished` in favour of default value
-        { data: { title, text, isPublished: false } },
-        info,
-      )
-    },
-    publish(parent, { id }, ctx, info) {
-      return ctx.db.mutation.updatePost(
-        {
-          where: { id },
-          data: { isPublished: true },
-        },
-        info,
-      )
-    },
-  },
-}
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
-  context: req => ({
-    ...req,
-    db: new Graphcool({
-      schemaPath: './database/schema.generated.graphql',
-      endpoint: 'http://localhost:60000/rest-wrapper/dev',
-      secret: 'mysecret123',
-    }),
-  }),
-})
+ })
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
